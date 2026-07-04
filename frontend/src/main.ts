2c5ca7e796069cbfb2f902e2245036105ad86131
maiajunok <lucykang01@gmail.com>
2026-07-05 01:55:15 +0900

    Add Vercel Web Analytics

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 import App from './App.vue'
 import router from './router'
 import '@/assets/style.css'
+
+inject()
 
 const app = createApp(App)
 
